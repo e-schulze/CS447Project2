4cf8f56b985f5b04ae8df77407f296ea05bc6753
Christopher Dang <devcdang@gmail.com>
2013-11-22 18:34:25 -0800

    Fixed maploader. No more hardcoded values. Less code too!

---
+++ CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Resources.java
@@ -0,0 +1,21 @@
+package edu.wsu.vancouver.ssdd;
+
+public class Resources {
+	/**
+	 * Stub out all the loading of images so it doesn't make the init method
+	 * very large
+	 */
+	public static void loadImages() {
+		jig.ResourceManager.loadImage("rsc/dungeontiles.gif");
+		jig.ResourceManager.loadImage("rsc/TestImage.png");
+	}
+
+	/**
+	 * Stub out all the loading of sounds so it doesn't make the init method
+	 * very large
+	 */
+	public static void loadSounds() {
+
+	}
+
+}
