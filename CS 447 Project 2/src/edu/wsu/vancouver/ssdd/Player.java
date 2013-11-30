b040ec0883937e0fb60aa2bdfd3f031f1374e4d0
Ethan Schulze <ethanschulze@gmail.com>
2013-11-29 23:58:26 -0800

    Issue 2 Resolved. The Player class is mostly complete. The only changes needed would be images for jumping and wall sliding.

--- CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Player.java
+++ CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Player.java
@@ -1,20 +1,25 @@
 package edu.wsu.vancouver.ssdd;
 
+import org.newdawn.slick.Animation;
+
 import jig.Entity;
+import jig.ResourceManager;
 import jig.Vector;
 
 public class Player extends Entity{
 
 	private int health;
 	private Vector vector;
-	
+	private String dir;
+
 	public Player(int x, int y, String dir){
 		super(x, y);
+		this.dir = dir;
 		if(dir.compareTo("Left") == 0){
-			//this.addImageWithBoundingBox(image); need to add the image of the player facing left
+			this.addImageWithBoundingBox(ResourceManager.getImage("rsc/images/PlayerStandingLeft.png"));
 		}
 		else if(dir.compareTo("Right") == 0){
-			//this.addImageWithBoundingBox(image); need to add the image of the player facing right
+			this.addImageWithBoundingBox(ResourceManager.getImage("rsc/images/PlayerStandingRight.png"));
 		}
 		setHealth(5); // might need to change
 		setVector(new Vector(0f, 0f)); // no initial movement
@@ -35,23 +40,45 @@ public class Player extends Entity{
 	public void setVector(Vector vector) {
 		this.vector = vector;
 	}
-	
+
 	public void decHealth(){
 		this.health--;
 	}
 	public void incHealth(){
 		this.health++;
 	}
-	
+
 	public void update(int delta){
 		translate(vector.scale(delta));
-		if(vector.getX() < 0){ // change the player's image to match the direction of movement (left)
-//			this.removeImage(image);
-//			this.addImageWithBoundingBox(image);
-		}
-		else if(vector.getX() < 0){ // change the player's image to match the direction of movement (left)
-//			this.removeImage(image);
-//			this.addImageWithBoundingBox(image);
+		if(vector.getY() == 0){
+			if(vector.getX() > 0){ // change the player's image to match the direction of movement (left)
+				if(dir.compareTo("Right") == 0){
+					this.removeImage(ResourceManager.getImage("rsc/images/PlayerStandingRight.png"));
+				}
+				else{
+					this.removeImage(ResourceManager.getImage("rsc/images/PlayerStandingLeft.png"));
+				}
+				this.addAnimation(new Animation(ResourceManager.getSpriteSheet("rsc/images/playerRight.png", 79, 90), 1000));
+			}
+			else if(vector.getX() < 0){ // change the player's image to match the direction of movement (left)
+				if(dir.compareTo("Right") == 0){
+					this.removeImage(ResourceManager.getImage("rsc/images/PlayerStandingRight.png"));
+				}
+				else{
+					this.removeImage(ResourceManager.getImage("rsc/images/PlayerStandingLeft.png"));
+				}
+				this.addAnimation(new Animation(ResourceManager.getSpriteSheet("rsc/images/playerLeft.png", 79, 90), 1000));
+			}
+			else if(vector.getX() == 0){
+				if(dir.compareTo("Right") == 0){
+					this.removeAnimation(new Animation(ResourceManager.getSpriteSheet("rsc/images/playerRight.png", 79, 90), 1000));
+					this.addImage(ResourceManager.getImage("rsc/images/PlayerStandingRight.png"));
+				}
+				else{
+					this.removeAnimation(new Animation(ResourceManager.getSpriteSheet("rsc/images/playerLeft.png", 79, 90), 1000));
+					this.addImage(ResourceManager.getImage("rsc/images/PlayerStandingLeft.png"));
+				}
+			}
 		}
 	}
 }
