357e07c730b3c550f20a81ec084eff812da4b47b
Sasha Demyanik <ttsvan@msn.com>
2013-12-03 16:34:58 -0800

    Added Menu classes and images and basic main.java framework.

--- CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Main.java
+++ CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Main.java
@@ -7,27 +7,95 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Main extends BasicGame {
 	public static final int logicInterval = 20;
+	private static final int START_UP = 1;
+	private static final int PLAYING = 2;
+	private static final int GAME_OVER = 3;
+	
+	private final int ScreenWidth = 600;
+	private final int ScreenHeight = 800;
+	private int gameState;
+	private int gameOverTimer;
+	
 
 	public Main(String title) {
 		super(title);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {
+	public void init(GameContainer container) throws SlickException {
+		startUp(container);
 	}
 
+	public void startUp(GameContainer container) {
+		gameState = START_UP;
+		container.setSoundOn(false);
+	}
+	
+	public void newGame(GameContainer container) {
+		gameState = PLAYING;
+		container.setSoundOn(true);
+	}
+	
+	public void gameOver() {
+		gameState = GAME_OVER;
+		gameOverTimer = 4000;
+	}
+	
+	public void makeMenu(){
+		//MenuLevel mLev = new MenuLevel(entity, x, y);
+	}
+	
 	@Override
-	public void update(GameContainer gc, int lastUpdateInterval) throws SlickException {
+	public void update(GameContainer container, int lastUpdateInterval) throws SlickException {
+		if (gameState == GAME_OVER) {
+			gameOverTimer -= lastUpdateInterval;
+			if (gameOverTimer <= 0)
+				startUp(container);
+		} else {
+			// get user input
+			Input input = container.getInput();
 
+			if (gameState == START_UP) {
+				if (input.isKeyDown(Input.KEY_SPACE))
+					newGame(container);
+			} else {
+				if (input.isKeyDown(Input.KEY_W)) {
+					
+				}
+				if (input.isKeyDown(Input.KEY_S)) {
+					
+				}
+				if (input.isKeyDown(Input.KEY_A)) {
+					
+				}
+				if (input.isKeyDown(Input.KEY_D)) {
+					
+				}
+				
+			}
+			
+		}
+
+		/*if (gameState == PLAYING && ) {
+			gameOver();
+		}*/
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		
+		switch(gameState){
+		case START_UP:
+			//render stuff
+		case PLAYING:
+			//render stuff
+		case GAME_OVER:
+			//render stuff
+		}
 	}
 
 	@Override
