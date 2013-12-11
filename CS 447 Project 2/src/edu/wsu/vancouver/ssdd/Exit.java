c6b4ab64dcb1ba1ab7bcd841143b81822c427875
Ethan Schulze <ethanschulze@gmail.com>
2013-12-11 01:00:02 -0800

    forgot two files in the last commit

---
+++ CS 447 Project 2/src/edu/wsu/vancouver/ssdd/Exit.java
@@ -0,0 +1,15 @@
+package edu.wsu.vancouver.ssdd;
+
+import jig.ResourceManager;
+
+public class Exit extends GameEntity{
+	
+	public Exit(EntityManager entityManager, Map map){
+		this(entityManager, map, 0f, 0f);
+	}
+	public Exit(EntityManager entityManager, Map map, float x, float y){
+		super(entityManager, x, y);
+		this.addImageWithBoundingBox(ResourceManager.getImage("images/door.png"));
+	}
+
+}
